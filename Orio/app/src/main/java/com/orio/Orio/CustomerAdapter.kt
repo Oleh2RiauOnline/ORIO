79efcc1408dbce8ca4dd195d39cc17e1d1c3fd98
Atikah Ilmi Mardatillah <52515478+atkhmi@users.noreply.github.com>
2021-01-13 17:14:40 +0700

    coba

--- Orio/app/src/main/java/com/orio/Orio/CustomerAdapter.kt
+++ Orio/app/src/main/java/com/orio/Orio/CustomerAdapter.kt
@@ -1,2 +1,23 @@
 package com.orio.Orio
 
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+
+register.setOnClickListener(object : View.OnClickListener {
+    override fun onClick(v: View) {
+        val txtUsername: String = username.getText().toString()
+        val txtName: String = name.getText().toString()
+        val txtEmail: String = email.getText().toString()
+        val txtPassword: String = password.getText().toString()
+        if (TextUtils.isEmpty(txtUsername) || TextUtils.isEmpty(txtName)
+            || TextUtils.isEmpty(txtEmail) || TextUtils.isEmpty(txtPassword)
+        ) {
+            Toast.makeText(this@RegisterActivity, "Empty credentials!", Toast.LENGTH_SHORT).show()
+        } else if (txtPassword.length < 6) {
+            Toast.makeText(this@RegisterActivity, "Password too short!", Toast.LENGTH_SHORT).show()
+        } else {
+            registerUser(txtUsername, txtName, txtEmail, txtPassword)
+        }
+    }
+})
--- Orio/app/src/main/java/com/orio/Orio/MainActivity.kt
+++ Orio/app/src/main/java/com/orio/Orio/MainActivity.kt
@@ -3,11 +3,25 @@ package com.orio.Orio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+private lateinit var register : Button
+private lateinit var login : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        register = findViewById (R.id.register)
+        login = findViewById (R.id.login)
+
+        register.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                startActivity(object intent(MainActivity.this, RegisterActivity.class))
+        })
     }
+}
 }

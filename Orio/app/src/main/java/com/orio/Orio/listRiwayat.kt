243d4f21669112d7db6b3da4bf43952cbd6b1247
raihanpcr <52515421+raihanpcr@users.noreply.github.com>
2021-01-22 21:42:10 +0700

    design

---
+++ Orio/app/src/main/java/com/orio/Orio/listRiwayat.kt
@@ -0,0 +1,18 @@
+package com.orio.Orio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.orio_keranjang_customer.*
+
+class listRiwayat : AppCompatActivity(){
+
+    val riwayat = arrayOf(
+        "Pisang Keju",
+        "Kembojo",
+        "Kripik Pisang",
+        "Makanan enak",
+    )
+}

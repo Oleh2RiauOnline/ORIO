1084d41202e9e1e746f253864585fb3299233903
raihanpcr <52515421+raihanpcr@users.noreply.github.com>
2021-01-22 21:57:18 +0700

    Update listRiwayat.kt

--- Orio/app/src/main/java/com/orio/Orio/listRiwayat.kt
+++ Orio/app/src/main/java/com/orio/Orio/listRiwayat.kt
@@ -2,10 +2,11 @@ package com.orio.Orio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import kotlinx.android.synthetic.main.orio_keranjang_customer.*
+import kotlinx.android.synthetic.main.riwayat_customer.*
 
 class listRiwayat : AppCompatActivity(){
 
@@ -15,4 +16,21 @@ class listRiwayat : AppCompatActivity(){
         "Kripik Pisang",
         "Makanan enak",
     )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        val arrayAdapter = ArrayAdapter(this, 
+            android.R.layout.simple_list_item_1, riwayat)
+        list_riwayat.adapter = arrayAdapter
+        
+        list_riwayat.setOnItemClickListener { parent : AdapterView<*>?, view : View?,
+                                              position : Int, id : Long->
+            Toast.makeText(
+                this, "Klik di "+ (position + 1) + " "+
+                        riwayat[position], Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 }

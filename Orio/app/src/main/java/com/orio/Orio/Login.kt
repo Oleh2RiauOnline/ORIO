332829af6888ff6802c2dcf6be5047dfcf766f3f
Atikah Ilmi Mardatillah <52515478+atkhmi@users.noreply.github.com>
2021-01-23 00:18:51 +0700

    Proses link halaman
    Khusus tampilan customer

---
+++ Orio/app/src/main/java/com/orio/Orio/Login.kt
@@ -0,0 +1,30 @@
+package com.orio.Orio
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.orio_login)
+
+        val button = findViewById<Button>(R.id.btn)
+        button.setOnClickListener{
+            val intent = Intent(this, OrioMenu::class.java)
+            startActivity(intent)
+        }
+        val textView = findViewById<TextView>(R.id.daftarregis)
+        textView.setOnClickListener{
+            val intent = Intent(this, OrioMenu::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+
+}
---
+++ Orio/app/src/main/java/com/orio/Orio/RiwayatPembelian.kt
@@ -0,0 +1,15 @@
+package com.orio.Orio
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RiwayatPembelian : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.riwayat_customer)
+
+
+    }
+
+
+}

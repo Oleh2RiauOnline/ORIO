90aaeff8ced166bc2ebe076f1a4e968cd821513f
Atikah Ilmi Mardatillah <52515478+atkhmi@users.noreply.github.com>
2021-01-24 21:05:28 +0700

    list

--- Orio/app/src/main/java/com/orio/Orio/Customer/KeranjangPembelian.kt
+++ Orio/app/src/main/java/com/orio/Orio/Customer/KeranjangPembelian.kt
@@ -1,14 +1,32 @@
 package com.orio.Orio.Customer
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.orio.Orio.R
+import kotlinx.android.synthetic.main.activity_show_customer.*
 
 class KeranjangPembelian : AppCompatActivity() {
+    val pembelian = arrayOf(
+        "Kue Bangkit 22-01-2021",
+        "Talam Durian 22-04-2021",
+        "Kue Bolu 02-03-2021",
+        "Kue Lapis 22-01-2021",
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.orio_keranjang_customer)
-
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, pembelian)
+        listView.adapter = arrayAdapter
+        listView.setOnItemClickListener{ parent: AdapterView<*>?, view: View?, posisi: Int, id: Long ->
+            Toast.makeText(
+                this, "Klik id: " + (posisi+1) + " " +
+                        pembelian[posisi], Toast.LENGTH_SHORT
+            ).show()
+        }
 
     }
 
--- Orio/app/src/main/java/com/orio/Orio/Customer/RiwayatPembelian.kt
+++ Orio/app/src/main/java/com/orio/Orio/Customer/RiwayatPembelian.kt
@@ -1,14 +1,33 @@
 package com.orio.Orio.Customer
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.orio.Orio.R
+import kotlinx.android.synthetic.main.activity_show_customer.*
 
 class RiwayatPembelian : AppCompatActivity() {
+    val history = arrayOf(
+        "Kue Bangkit",
+        "Talam Durian",
+        "Kue Bolu",
+        "Kue Lapis",
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.riwayat_customer)
 
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, history)
+        listView.adapter = arrayAdapter
+        listView.setOnItemClickListener{ parent: AdapterView<*>?, view: View?, posisi: Int, id: Long ->
+            Toast.makeText(
+                this, "Klik id: " + (posisi+1) + " " +
+                        history[posisi], Toast.LENGTH_SHORT
+            ).show()
+        }
 
     }
 
